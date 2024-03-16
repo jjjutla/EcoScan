97bbba235d74b0eba83cda74731c9347f34d9a33
acse-am9023 <artemiy.acse@gmail.com>
2024-03-16 13:37:10 +0000

    added iOS

---
+++ iOS/vechainCambridge/vechainCambridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  vechainCambridgeApp.swift
+//  vechainCambridge
+//
+//  Created by Artemiy Malyshau on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct vechainCambridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
