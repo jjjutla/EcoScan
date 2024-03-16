c4a9b8fe1718c2a3e8c2ed3023d14dec3c4a2dce
Jeevan Jutla <jj@JJPC.local>
2024-03-16 23:08:51 +0000

    init deployed erc-20

---
+++ contracts/contracts/ERC-20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract RewardToken is Initializable, ERC20Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    function initialize() public initializer {
+        __ERC20_init("RewardToken", "RWT");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
